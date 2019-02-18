6008c46e80bc2526e0b8f4efd07e5a53d142fc13
Dhiogo Brustolin <dhiogorb@gmail.com>
2019-02-18 09:22:01 -0300

    Add new objects do index

--- ts/index.ts
+++ ts/index.ts
@@ -3,3 +3,5 @@ export { HttpHandler } from "./HttpHandler";
 export { HttpSession } from "./HttpSession";
 export { StaticHandler } from "./StaticHandler";
 export { AppSettings } from "./AppSettings";
+export { HttpContext } from "./HttpContext"
+export { HttpServerMiddlewareFunction, HttpServerMiddleware } from "./HttpMiddleware"
